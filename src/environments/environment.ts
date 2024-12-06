125ffb63ef5527d84cf92e176b26ad4985fb09f2
Arkalithe <arkalithe@gmail.com>
2024-12-06 10:16:42 +0100

    modification angular.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  API_URL: 'https://electric-business-a977082804f7.herokuapp.com/api',
+  LOCALISATION_KEYS: {
+    TOKEN: 'token',
+  },
+};
